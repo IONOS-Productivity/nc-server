9fb303fcd6725def35a09d061af16083563aedcc
Thomas Lehmann <t.lehmann@strato-rz.de>
2023-11-14 18:13:13 +0100

    (C11N) config: Add IONOS first time configure script first-time-configure.sh

---
+++ config/first-time-configure.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+function ooc() {
+	php occ \
+		"${@}"
+}
+
+function main() {
+	# No such app enabled: password_policy
+	# ooc app:disable password_policy
+
+	# test required apps are set
+	REQS=( php )
+
+	for REQ in "${REQS[@]}"
+	do
+		which "${REQ}" >/dev/null
+		if [ ! $? -eq 0 ]; then
+			echo "ERROR: requirement '${REQ}' is missing"
+			exit 1
+		fi
+	done
+
+	echo "Configure NextCloud basics"
+
+	if [[ $(ooc status --output json | jq '.installed') != "true" ]]; then
+		echo "NextCloud is not installed, abort"
+		exit 1
+	fi
+
+	ooc config:system:set lookup_server --value=""
+	ooc user:setting admin settings email admin@example.net
+
+	echo "Configure theming"
+
+	ooc theming:config name "EasyStorage"
+	ooc theming:config color "#003D8F"
+	ooc theming:config disable-user-theming true
+	ooc config:app:set theming backgroundMime --value backgroundColor
+
+	echo "Disable apps"
+
+	local disable_apps=(
+		"activity"
+		"circles"
+		"comments"
+		"contactsinteraction"
+		"dashboard"
+		"files_sharing"
+		"files_versions"
+		"firstrunwizard"
+		"logreader"
+		"nextcloud_announcements"
+		"privacy"
+		"recommendations"
+		"related_resources"
+		"serverinfo"
+		"settings"
+		"sharebymail"
+		"support"
+		"survey_client"
+		"systemtags"
+		"updatenotification"
+		"user_status"
+		"weather_status"
+		"workflowengine"
+	)
+
+	for app in ${disable_apps[@]}; do
+		echo "Disable app '${app}' ..."
+		ooc app:disable "${app}"
+	done
+}
+
+main ${@}
