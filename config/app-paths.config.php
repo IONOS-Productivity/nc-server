913e257fd45bb867f1748d13f472dac8bd118e99
Matthias Sauer <sauer@strato-rz.de>
2023-12-14 17:58:41 +0100

    [format] config/app-paths.config.php respect editorconfig

--- config/app-paths.config.php
+++ config/app-paths.config.php
@@ -1,15 +1,15 @@
 <?php
 $CONFIG = [
-  'apps_paths' => [
-    [
-      'path' => '/var/www/html/apps',
-      'url' => '/apps',
-      'writable' => true,
-    ],
-    [
-      'path' => '/var/www/html/custom-apps',
-      'url' => '/custom-apps',
-      'writable' => true,
-    ],
-  ],
+	'apps_paths' => [
+		[
+			'path' => '/var/www/html/apps',
+			'url' => '/apps',
+			'writable' => true,
+		],
+		[
+			'path' => '/var/www/html/custom-apps',
+			'url' => '/custom-apps',
+			'writable' => true,
+		],
+	],
 ];
