2a57bf214672c51db723c263a48d6441bffda7c9
Thomas Lehmann <t.lehmann@strato-rz.de>
2023-12-14 14:46:34 +0100

    (C11N) config: disable sections: hide TwoFactor, WebAuthn, Password
    This removes the the TwoFactor, WebAuthn, Password left navigation
    entries on the settings page.
    
    Goal: limit functionality on the settings page. Since authentication
    shall be done through OpenID connect, this should not be configurable
    by the user locally.
    
    Terminology:
    
    - "Sections" refer to the navigation element to reach a setting page
    - "Settings" refer to the content elements on the page

--- config/disable-sections.config.php
+++ config/disable-sections.config.php
@@ -6,6 +6,9 @@ $CONFIG = [
     'settings' => [
       'OCA\\Settings\\Sections\\Personal\\Availability',
       'OCA\\Settings\\Sections\\Personal\\PersonalInfo',
+      'OCA\\Settings\\Settings\\Personal\\Security\\WebAuthn',
+      'OCA\\Settings\\Settings\\Personal\\Security\\Password',
+      'OCA\\Settings\\Settings\\Personal\\Security\\TwoFactor',
     ],
   ],
 ];
