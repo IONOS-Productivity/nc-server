92719cf2cc1b122c8dbaf94896c368270cc92293
Thomas Lehmann <t.lehmann@strato.de>
2024-07-05 16:22:38 +0200

    IONOS(occ): add redirect config and mask sensitive config values in occ output
    Adds 'redirects' system config key: maps URL path regexps to app route
    locators, configured entirely in config.php rather than Apache/nginx.
    
      Example: "redirects" => ["^/path/regexp" => "an.apps.controller.route"]
    
    Note: the <sensitive> masking in config:app:set was already upstreamed to v32.
    
    Backports: 959dcc0c1b2 c64d946bca5
    (cherry picked from commit e7137a700109380f7e459b6fcc70d78557e55edc)

--- lib/base.php
+++ lib/base.php
@@ -1152,7 +1152,27 @@ class OC {
 					$appManager->loadApps(['filesystem', 'logging']);
 					$appManager->loadApps();
 				}
-				Server::get(\OC\Route\Router::class)->match($request->getRawPathInfo());
+				$requestPath = $request->getRawPathInfo();
+				$redirects = $systemConfig->getValue('redirects', []);
+
+				if ($redirects) {
+					foreach ($redirects as $fromPattern => $toLocator) {
+						if (!preg_match('/' . $fromPattern . '/', $requestPath)) {
+							continue;
+						}
+
+						try {
+							$targetLocation = Server::get(IURLGenerator::class)->linkToRouteAbsolute($toLocator);
+							header('Location: ' . $targetLocation);
+							return;
+						} catch (\Exception) {
+							// In case of container exceptions or
+							// route not found exceptions we proceed as usual.
+						}
+					}
+				}
+
+				Server::get(\OC\Route\Router::class)->match($requestPath);
 				return;
 			} catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
 				//header('HTTP/1.0 404 Not Found');
